dba8e5843148a4ecc20b497175cae3d5171561b8
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-10-13 10:13:00 +0200

    Updated tests

--- src/tests/Equinor.ProCoSys.PO.MainApi.Tests/Permission/MainApiPermissionServiceTests.cs
+++ src/tests/Equinor.ProCoSys.PO.MainApi.Tests/Permission/MainApiPermissionServiceTests.cs
@@ -65,6 +65,44 @@ namespace Equinor.ProCoSys.PO.MainApi.Tests.Permission
             // Assert
             Assert.AreEqual(0, result.Count);
         }
+        
+        [TestMethod]
+        public async Task GetAllProjectsAsync_ShouldReturnThreeProjects_OnValidPlant()
+        {
+            // Arrange
+            _mainApiClient
+                .SetupSequence(x => x.QueryAndDeserializeAsync<List<ProCoSysProject>>(It.IsAny<string>()))
+                .Returns(Task.FromResult(new List<ProCoSysProject>{ new ProCoSysProject(), new ProCoSysProject() }));
+            // Act
+            var result = await _dut.GetAllProjectsAsync(_plant);
+
+            // Assert
+            Assert.AreEqual(2, result.Count);
+        }
+
+        [TestMethod]
+        public async Task GetAllProjectsAsync_ShouldReturnNoProjects_OnValidPlant()
+        {
+            // Arrange
+            _mainApiClient
+                .SetupSequence(x => x.QueryAndDeserializeAsync<List<ProCoSysProject>>(It.IsAny<string>()))
+                .Returns(Task.FromResult(new List<ProCoSysProject>()));
+            // Act
+            var result = await _dut.GetAllProjectsAsync(_plant);
+
+            // Assert
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public async Task GetAllProjectsAsync_ShouldReturnNoProjects_OnInValidPlant()
+        {
+            // Act
+            var result = await _dut.GetAllProjectsAsync("INVALIDPLANT");
+
+            // Assert
+            Assert.AreEqual(0, result.Count);
+        }
 
         [TestMethod]
         public async Task GetContentRestrictions_ShouldReturnThreePermissions_OnValidPlant()
