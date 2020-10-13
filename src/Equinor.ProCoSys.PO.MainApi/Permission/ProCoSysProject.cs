2f9466acc868e93913bd8f8537e67de14ff01f6f
Jan Inge Dalsbø <JDALSB@equinor.com>
2020-10-13 10:08:42 +0200

    Refactor IPermissionApiService -> GetAllProjectsAsync now get all projects, also those where user don't have access. New property ProCoSysProject.HasAccess tell if user has access

---
+++ src/Equinor.ProCoSys.PO.MainApi/Permission/ProCoSysProject.cs
@@ -0,0 +1,9 @@
+﻿namespace Equinor.ProCoSys.PO.MainApi.Permission
+{
+    public class ProCoSysProject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool HasAccess { get; set; }
+    }
+}
